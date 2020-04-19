6f53cb4702e3cf3c3aa747f00efd9ddd9c635a36
Suraj <surajchan68@gmai.com>
2020-04-19 15:17:50 +0545

    Creating firebase db and upadate data with key  manually from Client

--- views/js/firebaseAuth.js
+++ views/js/firebaseAuth.js
@@ -1,35 +1,52 @@
 function checkIfLoggedIn() {
-    if (localStorage.getItem('firebase_idToken')) {
-        // do nothing
-        document.getElementById('google-pic')
-            .setAttribute('src', localStorage.getItem('google-photo'));
+    firebase.auth().onAuthStateChanged(user => {
+        // if (localStorage.getItem('firebase_idToken')) {
+        if (user) {
+            // do logged in stuff
+            console.log('user sign in');
+            // console.log(user);
 
-        document.getElementById('google-signin')
-            .setAttribute('style', 'display:none; visibility: hidden');
+            let photoURL = user.photoURL;
 
-        document.getElementById('signout')
-            .setAttribute('style', 'display: inline-block; visibility: visible');
-    } else {
-        document.getElementById('google-signin')
-            .setAttribute('style', 'display:inline-block; visibility: visible');
+            document.getElementById('google-pic')
+                .setAttribute('src', photoURL);
+
+            // document.getElementById('google-pic')
+            //     .setAttribute('src', localStorage.getItem('google-photo'));
+
+            document.getElementById('google-signin')
+                .setAttribute('style', 'display:none; visibility: hidden');
+
+            document.getElementById('signout')
+                .setAttribute('style', 'display: inline-block; visibility: visible');
+        } else {
+            // do not logged in stuff
+            console.log('user log out');
+            document.getElementById('google-signin')
+                .setAttribute('style', 'display:inline-block; visibility: visible');
 
 
-        document.getElementById('signout')
-            .setAttribute('style', 'display: none; visibility: hidden');
-    }
-
+            document.getElementById('signout')
+                .setAttribute('style', 'display: none; visibility: hidden');
+        }
+    });
+    // let user = firebase.auth().currentUser;
+    // console.log(user); // Null is output
 }
-checkIfLoggedIn();
+
+window.onload = () => {
+    checkIfLoggedIn();
+}
 
 function signOut() {
-    localStorage.removeItem('firebase_idToken');
-    localStorage.removeItem('google-photo');
+
+    firebase.auth().signOut();
+
+    // localStorage.removeItem('firebase_idToken');
+    // localStorage.removeItem('google-photo');
 
     document.getElementById('google-pic')
         .setAttribute('src', '');
-
-    checkIfLoggedIn();
-
 }
 
 function signInWithGoogle() {
@@ -43,10 +60,11 @@ function signInWithGoogle() {
 
             // Set IdToken in localStroage of Browser
             const idToken = result.credential.idToken;
-            localStorage.setItem('firebase_idToken', idToken);
+
+            // localStorage.setItem('firebase_idToken', idToken);
 
             const photo = result.user.photoURL;
-            localStorage.setItem('google-photo', photo);
+            // localStorage.setItem('google-photo', photo);
 
             document.getElementById('google-email').innerHTML = result.user.email;
 
